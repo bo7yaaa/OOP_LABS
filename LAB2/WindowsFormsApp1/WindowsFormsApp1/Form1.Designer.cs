8dbf7ab566d3455f3649dab1557e9cd906742d80
bo7yaaa <129153202+bo7yaaa@users.noreply.github.com>
2024-03-25 16:38:11 +0200

    Add files via upload

---
+++ LAB2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Коло";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Квадрат";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Прямокутник";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Змінити градус фігури";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 301);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Повернути значення";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(159, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(593, 294);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 219);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 136);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(108, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Очистити поле";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 245);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(120, 23);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Зміна розмірів";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 274);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 348);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "LAB2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Text changed: " + textBox1.Text);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ LAB2/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // Перечисление для типов фигур
+        private enum ShapeType
+        {
+            Square,
+            Circle,
+            Rectangle
+        }
+
+        // Класс для представления фигур
+        private class Shape
+        {
+            public ShapeType Type { get; set; } // Тип фигуры
+            public Rectangle Bounds { get; set; } // Границы фигуры
+            public int RotationAngle { get; set; } // Угол поворота фигуры
+        }
+
+        private List<Shape> shapes = new List<Shape>(); // Список для хранения фигур
+        private Point dragStart; // Точка начала перетаскивания
+        private bool isDragging = false; // Флаг перетаскивания
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Подписываемся на события мыши для панели
+            panel1.MouseMove += Panel1_MouseMove;
+            panel1.MouseDown += Panel1_MouseDown;
+            panel1.MouseUp += Panel1_MouseUp;
+        }
+
+        // Обработчики нажатия на кнопки создания фигур
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Shape square = new Shape
+            {
+                Type = ShapeType.Square,
+                Bounds = new Rectangle(50, 50, 100, 100),
+                RotationAngle = 0
+            };
+            shapes.Add(square);
+            panel1.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Shape circle = new Shape
+            {
+                Type = ShapeType.Circle,
+                Bounds = new Rectangle(200, 50, 100, 100),
+                RotationAngle = 0
+            };
+            shapes.Add(circle);
+            panel1.Invalidate();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Shape rectangle = new Shape
+            {
+                Type = ShapeType.Rectangle,
+                Bounds = new Rectangle(350, 50, 150, 100),
+                RotationAngle = 0
+            };
+            shapes.Add(rectangle);
+            panel1.Invalidate();
+        }
+
+        // Обработчики событий мыши для панели
+        private void Panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging && e.Button == MouseButtons.Left)
+            {
+                foreach (var shape in shapes)
+                {
+                    if (shape.Bounds.Contains(dragStart))
+                    {
+                        int dx = e.X - dragStart.X;
+                        int dy = e.Y - dragStart.Y;
+                        shape.Bounds = new Rectangle(shape.Bounds.X + dx, shape.Bounds.Y + dy, shape.Bounds.Width, shape.Bounds.Height);
+                        dragStart = e.Location;
+                        panel1.Invalidate();
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void Panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+            dragStart = e.Location;
+        }
+
+        private void Panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        // Обработчики рисования на панели
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var shape in shapes)
+            {
+                Graphics g = e.Graphics;
+                switch (shape.Type)
+                {
+                    case ShapeType.Square:
+                        // Рисуем квадрат
+                        g.TranslateTransform(shape.Bounds.Left + shape.Bounds.Width / 2, shape.Bounds.Top + shape.Bounds.Height / 2);
+                        g.RotateTransform(shape.RotationAngle);
+                        g.TranslateTransform(-(shape.Bounds.Left + shape.Bounds.Width / 2), -(shape.Bounds.Top + shape.Bounds.Height / 2));
+                        g.DrawRectangle(Pens.Black, shape.Bounds);
+                        g.ResetTransform();
+                        break;
+                    case ShapeType.Circle:
+                        // Рисуем круг
+                        g.TranslateTransform(shape.Bounds.Left + shape.Bounds.Width / 2, shape.Bounds.Top + shape.Bounds.Height / 2);
+                        g.RotateTransform(shape.RotationAngle);
+                        g.TranslateTransform(-(shape.Bounds.Left + shape.Bounds.Width / 2), -(shape.Bounds.Top + shape.Bounds.Height / 2));
+                        g.DrawEllipse(Pens.Black, shape.Bounds);
+                        g.ResetTransform();
+                        break;
+                    case ShapeType.Rectangle:
+                        // Рисуем прямоугольник
+                        g.TranslateTransform(shape.Bounds.Left + shape.Bounds.Width / 2, shape.Bounds.Top + shape.Bounds.Height / 2);
+                        g.RotateTransform(shape.RotationAngle);
+                        g.TranslateTransform(-(shape.Bounds.Left + shape.Bounds.Width / 2), -(shape.Bounds.Top + shape.Bounds.Height / 2));
+                        g.DrawRectangle(Pens.Black, shape.Bounds);
+                        g.ResetTransform();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        // Обработчик нажатия на кнопку изменения угла поворота
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Пытаемся преобразовать текст введенный в текстовое поле в число
+            if (int.TryParse(textBox1.Text, out int angle))
+            {
+                // Устанавливаем угол поворота для всех фигур введенное значение
+                foreach (var shape in shapes)
+                {
+                    shape.RotationAngle = angle;
+                }
+                panel1.Invalidate(); // Перерисовываем панель
+            }
+            else
+            {
+                MessageBox.Show("Invalid angle!"); // Выводим сообщение об ошибке, если угол введен некорректно
+            }
+        }
+
+        // Обработчик нажатия на кнопку сброса угла поворота
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Сбрасываем угол поворота для всех фигур
+            foreach (var shape in shapes)
+            {
+                shape.RotationAngle = 0;
+            }
+            panel1.Invalidate(); // Перерисовываем панель
+        }
+
+        // Обработчик нажатия на кнопку очистки панели
+        private void button6_Click(object sender, EventArgs e)
+        {
+            shapes.Clear(); // Очищаем список фигур
+            panel1.Invalidate(); // Перерисовываем панель
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // Пытаемся преобразовать текст введенный в textBox2 в число
+            if (int.TryParse(textBox2.Text, out int newSize))
+            {
+                // Изменяем размеры всех фигур на новые значения
+                foreach (var shape in shapes)
+                {
+                    // Проверяем тип фигуры и устанавливаем новые размеры
+                    switch (shape.Type)
+                    {
+                        case ShapeType.Square:
+                            // Если фигура квадрат, то устанавливаем новый размер
+                            shape.Bounds = new Rectangle(shape.Bounds.Location, new Size(newSize, newSize));
+                            break;
+                        case ShapeType.Circle:
+                            // Если фигура круг, то устанавливаем новый радиус
+                            shape.Bounds = new Rectangle(shape.Bounds.Location, new Size(newSize, newSize));
+                            break;
+                        case ShapeType.Rectangle:
+                            // Если фигура прямоугольник, то изменяем ширину и высоту независимо
+                            shape.Bounds = new Rectangle(shape.Bounds.Location, new Size(shape.Bounds.Width, newSize));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                panel1.Invalidate(); // Перерисовываем панель
+            }
+            else
+            {
+                MessageBox.Show("Invalid size!"); // Выводим сообщение об ошибке, если размер введен некорректно
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
