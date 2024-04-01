9f4d03419725a29e6466c7e3a5eded4a50f68856
bo7yaaa <129153202+bo7yaaa@users.noreply.github.com>
2024-04-01 19:04:01 +0300

    Add files via upload

---
+++ LAB5/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string inputText = textBox1.Text;
+            string resultText = RemoveSpaces(inputText);
+            MessageBox.Show(resultText, "Результат");
+        }
+
+        private string RemoveSpaces(string input)
+        {
+            // Розділити введений текст за всіма символами пропуску
+            string[] parts = input.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Повернути результуючий рядок, об'єднавши частини без пропусків
+            return string.Join("", parts);
+        }
+
+    }
+}
