cbc1773285b81ef9c76316650c372af6ef6c7b43
bo7yaaa <129153202+bo7yaaa@users.noreply.github.com>
2024-04-01 18:20:59 +0300

    Add files via upload

---
+++ LAB4/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ArrayOperations
+{
+    partial class MainForm
+    {
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(153, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(153, 38);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(260, 23);
+            this.calculateButton.TabIndex = 1;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(12, 74);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 295);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+
+        #endregion
+    }
+}
+
---
+++ LAB4/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ArrayOperations
+{
+    public partial class MainForm : Form
+    {
+        private TextBox textBox1;
+        private Button calculateButton;
+        private Label resultLabel;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            // Розрахунок для одновимірного масиву
+            double[] array = textBox1.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                          .Select(double.Parse)
+                                          .ToArray();
+
+            // а) сума від’ємних елементів масиву
+            double negativeSum = array.Where(x => x < 0).Sum();
+
+            // б) добуток елементів масиву, розташованих між максимальним і мінімальним елементом
+            double min = array.Min();
+            double max = array.Max();
+            double product = 1;
+
+            bool foundMin = false;
+            bool foundMax = false;
+
+            foreach (double element in array)
+            {
+                if (element == min)
+                {
+                    foundMin = true;
+                    continue;
+                }
+
+                if (element == max)
+                {
+                    foundMax = true;
+                    break;
+                }
+
+                if (foundMin && !foundMax)
+                {
+                    product *= element;
+                }
+            }
+
+            // Впорядкування елементів масиву за зростанням
+            Array.Sort(array);
+
+            // Виведення результатів в Label
+            resultLabel.Text = $"а) Сума від’ємних елементів масиву: {negativeSum}\n";
+            resultLabel.Text += $"б) Добуток елементів масиву, розташованих між максимальним і мінімальним елементом: {product}\n";
+            resultLabel.Text += $"Впорядкований масив: {string.Join(" ", array)}\n";
+
+            // Двовимірний масив
+            int[,] twoDimensionalArray = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            // Вивести на екран увесь масив
+            resultLabel.Text += "Двовимірний масив:\n";
+            for (int i = 0; i < twoDimensionalArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < twoDimensionalArray.GetLength(1); j++)
+                {
+                    resultLabel.Text += twoDimensionalArray[i, j] + " ";
+                }
+                resultLabel.Text += "\n";
+            }
+
+            // Вивести на екран елемент, розміщений у правому верхньому куті масиву
+            resultLabel.Text += $"Елемент, розміщений у правому верхньому куті масиву: {twoDimensionalArray[0, twoDimensionalArray.GetLength(1) - 1]}\n";
+
+            // Вивести на екран елемент, розміщений у лівому нижньому куті масиву
+            resultLabel.Text += $"Елемент, розміщений у лівому нижньому куті масиву: {twoDimensionalArray[twoDimensionalArray.GetLength(0) - 1, 0]}\n";
+        }
+    }
+}
